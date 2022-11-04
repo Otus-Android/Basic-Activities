7fdfd4be61bff7953ce9d76b340f8bec986627e0
AzaliaAknazarova <azalika.aknazarova@icloud.com>
2022-11-04 21:05:33 +0500

    Рефакторинг

--- app/src/main/java/otus/gpb/homework/activities/ActivityA.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityA.kt
@@ -2,6 +2,7 @@ package otus.gpb.homework.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,6 +16,12 @@ class ActivityA : AppCompatActivity() {
     fun onClickGoB(view: View){
         val intent = Intent(this, ActivityB::class.java)
             .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
         startActivity(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        Log.e("test", "onNewIntent")
     }
 }
--- app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
@@ -17,18 +17,9 @@ class ActivityC : AppCompatActivity() {
         startActivity(intent)
     }
 
-    // в кейсе с переходом в ActivityD (onClickGoD) не понятно как действовать
-    // флаг FLAG_ACTIVITY_CLEAR_TASK - отлично сработал бы если бы у нас была ActivityD корневой
-    //, иначе это не сработает как в кеейсе
-
-    //конструкция .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK) также хороший вариант
-    // но по тз не подходит, так как FLAG_ACTIVITY_NEW_TASK открывает новый стек
-
-    // как вариант думала не сохранять в стеке, но это не эффективно и также по тз не проходит
-
     fun onClickGoD(view: View){
         val intent = Intent(this, ActivityD::class.java)
-            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         startActivity(intent)
     }
 
