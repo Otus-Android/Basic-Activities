7934db704fad3009598fca80eb0786b03c5b30bf
Andrey Efimov <aiefimov@sberbank.ru>
2023-05-29 21:55:54 +0300

    Task2

--- app/src/main/java/otus/gpb/homework/activities/ActivityB.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityB.kt
@@ -1,14 +1,24 @@
 package otus.gpb.homework.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class ActivityB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_b)
 
-        findViewById<Button>(R.id.buttonActivityBOpenC).setOnClickListener {}
+        findViewById<Button>(R.id.buttonActivityBOpenC).setOnClickListener {
+            val intent = Intent(this, ActivityC::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "ActivityB onDestroy", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
@@ -1,17 +1,35 @@
 package otus.gpb.homework.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class ActivityC : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_c)
 
-        findViewById<Button>(R.id.buttonActivityCOpenA).setOnClickListener {}
-        findViewById<Button>(R.id.buttonActivityCOpenD).setOnClickListener {}
-        findViewById<Button>(R.id.buttonActivityCClose).setOnClickListener {}
-        findViewById<Button>(R.id.buttonActivityCCloseStack).setOnClickListener {}
+        findViewById<Button>(R.id.buttonActivityCOpenA).setOnClickListener {
+            val intent = Intent(this, ActivityA::class.java)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.buttonActivityCOpenD).setOnClickListener {
+            val intent = Intent(this, ActivityD::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK and Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+        findViewById<Button>(R.id.buttonActivityCClose).setOnClickListener {
+            finish()
+        }
+        findViewById<Button>(R.id.buttonActivityCCloseStack).setOnClickListener {
+            finishAffinity()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "ActivityC onDestroy", Toast.LENGTH_SHORT).show()
     }
 }
