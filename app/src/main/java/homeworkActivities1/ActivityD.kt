6783a0d47036a9d6af7d455ef1cd100b231a790b
Сычев Михаил Валерьевич <msychev@mts.ru>
2023-11-04 16:41:48 +0300

    [HOMEWORK_ACTIVITIES_1]

---
+++ app/src/main/java/homeworkActivities1/ActivityD.kt
@@ -0,0 +1,11 @@
+package homeworkActivities1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import otus.gpb.homework.activities.R
+
+class ActivityD : AppCompatActivity(R.layout.activity_d) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
