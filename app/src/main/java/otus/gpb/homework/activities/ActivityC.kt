9495e28d84c3fe4118232ca771aa4b57236a04ff
viktoriurkov <victor.iurkov@gmail.com>
2023-11-06 21:10:55 +0300

    [android-1]: code review fixes

--- app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityC.kt
@@ -22,7 +22,7 @@ class ActivityC : AppCompatActivity(R.layout.activity_c) {
 
     private fun openActivityD() {
         val intent = Intent(this, ActivityD::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         }
         startActivity(intent)
     }
