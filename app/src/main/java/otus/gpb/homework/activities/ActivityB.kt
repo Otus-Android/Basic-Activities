c9d459286dfa82721b4b52f5ed1fea386efb051b
glasolin <glasolin@gmail.com>
2023-11-14 20:18:27 +0300

    - added ActivityB

--- app/src/main/java/otus/gpb/homework/activities/ActivityB.kt
+++ app/src/main/java/otus/gpb/homework/activities/ActivityB.kt
@@ -12,18 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import otus.gpb.homework.activities.ui.theme.ActivitiesTheme
 
-class ActivityA : ComponentActivity(R.layout.activity_a) {
-    lateinit var button:Button
+class ActivityB : ComponentActivity(R.layout.activity_b) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val id=(applicationContext as App).id
-        button=findViewById(R.id.openb)
     }
 
-    override fun onStart() {
-        super.onStart()
-        button.onClickListener {
 
-        }
-    }
 }
